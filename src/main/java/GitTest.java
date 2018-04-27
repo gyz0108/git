3d393f585efbf2146dc40787cf820754f23eb8ce
gao <15733032023@163.com>
2018-04-27 21:19:36 +0800

    初始化项目1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("new Git");
         System.out.println("网页添加");
+        System.out.println("第三次提交");
     }
 }
